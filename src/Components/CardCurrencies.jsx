3dffebbe9f384b42bf60381ca8381f5b7af10b0d
Yash <gau.yash05@gmail.com>
2023-08-28 13:09:55 +0530

    Styling added for topcurrencies component

--- src/Components/CardCurrencies.jsx
+++ src/Components/CardCurrencies.jsx
@@ -2,57 +2,57 @@ import React from "react";
 
 const CardCurrencies = () => {
   return (
-    <div className="card_layout">
-      <div className="card card_currencies">
-        <div>
+    <div className="card_layout flex">
+      <div className="card shadow">
+        <div className="flex items-center justify-between">
           <h3 className="card_heading">1. Bitcoin</h3>
           <img src="/public/vite.svg" alt="" width="20px" />
         </div>
-        <div>
+        <div className="flex flex-col gap-4 pt-6">
           <p className="card_price">Price: 26.2k</p>
           <p className="card_cap">Market Cap: 510.4B</p>
           <p className="card_change">Daily Change: -0.2%</p>
         </div>
       </div>
-      <div className="card card_currencies">
-        <div>
+      <div className="card shadow">
+        <div className="flex items-center justify-between">
           <h3 className="card_heading">1. Bitcoin</h3>
           <img src="/public/vite.svg" alt="" width="20px" />
         </div>
-        <div>
+        <div className="flex flex-col gap-4 pt-6">
           <p className="card_price">Price: 26.2k</p>
           <p className="card_cap">Market Cap: 510.4B</p>
           <p className="card_change">Daily Change: -0.2%</p>
         </div>
       </div>
-      <div className="card card_currencies">
-        <div>
+      <div className="card shadow">
+        <div className="flex items-center justify-between">
           <h3 className="card_heading">1. Bitcoin</h3>
           <img src="/public/vite.svg" alt="" width="20px" />
         </div>
-        <div>
+        <div className="flex flex-col gap-4 pt-6">
           <p className="card_price">Price: 26.2k</p>
           <p className="card_cap">Market Cap: 510.4B</p>
           <p className="card_change">Daily Change: -0.2%</p>
         </div>
       </div>
-      <div className="card card_currencies">
-        <div>
+      <div className="card shadow">
+        <div className="flex items-center justify-between">
           <h3 className="card_heading">1. Bitcoin</h3>
           <img src="/public/vite.svg" alt="" width="20px" />
         </div>
-        <div>
+        <div className="flex flex-col gap-4 pt-6">
           <p className="card_price">Price: 26.2k</p>
           <p className="card_cap">Market Cap: 510.4B</p>
           <p className="card_change">Daily Change: -0.2%</p>
         </div>
       </div>
-      <div className="card card_currencies">
-        <div>
+      <div className="card shadow">
+        <div className="flex items-center justify-between">
           <h3 className="card_heading">1. Bitcoin</h3>
           <img src="/public/vite.svg" alt="" width="20px" />
         </div>
-        <div>
+        <div className="flex flex-col gap-4 pt-6">
           <p className="card_price">Price: 26.2k</p>
           <p className="card_cap">Market Cap: 510.4B</p>
           <p className="card_change">Daily Change: -0.2%</p>
--- src/Components/TopCurrencies.jsx
+++ src/Components/TopCurrencies.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import CardCurrencies from "./CardCurrencies";
+import { Icon } from '@iconify/react';
+import './../styles/TopCurrencies.scss'
 
 const TopCurrencies = () => {
   return (
     <section>
-      <div>
-        <h2 className="heading">Top 10 Cryptocurrencies in the World</h2>
-        <Link to="/cryptocurrencies">Show More</Link>
+      <div className="flex items-center justify-between">
+        <h2 className="heading font-medium">Top 10 Cryptocurrencies in the World</h2>
+        <Link to="/cryptocurrencies"><Icon icon="fluent-mdl2:go" width="15" /></Link>
       </div>
       <CardCurrencies />
     </section>
