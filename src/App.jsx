0d898035e2a4a0f1d004bb1c3b0dcaaac962efa7
Yash <gau.yash05@gmail.com>
2023-08-28 09:38:30 +0530

    Cryptocurrencies Page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom"
 import MainLayout from "./Components/Layout/MainLayout"
 import Home from "./Pages/Home"
+import CryptoCurrencies from "./Pages/CryptoCurrencies"
+import News from "./Pages/News"
 
 function App() {
 
@@ -9,6 +11,8 @@ function App() {
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<Home />}/>
+        <Route path="cryptocurrencies" element={<CryptoCurrencies />}/>
+        <Route path="news" element={<News />}/>
       </Route>
     </Routes>
     </>
---
+++ src/Pages/CryptoCurrencies.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import CardCurrencies from "../Components/CardCurrencies";
+
+const CryptoCurrencies = () => {
+  return (
+    <section>
+      <form>
+        <input type="text" />
+      </form>
+
+      <CardCurrencies />
+    </section>
+  );
+};
+
+export default CryptoCurrencies;
