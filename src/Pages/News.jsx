ca1c9716a4e6400fc379c3c634af57d3646b56ee
Yash <gau.yash05@gmail.com>
2023-08-28 09:42:56 +0530

    Made News Page

--- src/Pages/News.jsx
+++ src/Pages/News.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import CardNews from "../Components/CardNews";
 
 const News = () => {
   return (
-    <div>News</div>
-  )
-}
+    <section>
+      <form>
+        <select name="" id="">
+          <option hidden value="">
+            Select a Crypto
+          </option>
+          <option value="">CryptoCurrency</option>
+          <option value="">Bitcoin</option>
+          <option value="">Dogecoin</option>
+          <option value="">Ethereum</option>
+          <option value="">BNB</option>
+        </select>
+      </form>
 
-export default News
+      <CardNews />
+    </section>
+  );
+};
+
+export default News;
