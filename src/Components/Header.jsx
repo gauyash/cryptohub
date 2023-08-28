f6119cd36e16e622241f99db513ffd5c9c89a0a3
Yash <gau.yash05@gmail.com>
2023-08-28 10:01:38 +0530

    Added Styling for header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Icon } from "@iconify/react";
+import { Link } from "react-router-dom";
+import './../styles/Header.scss'
 
 const Header = () => {
   return (
-    <header className="bg-blue-400">
-      <div className="logo_box">
-        <img src="/public/images/logo.png" alt="" />
-        <h1>CryptoHub</h1>
+    <header className="bg-black text-white flex items-center justify-between px-5 py-7">
+      <div className="logo_box flex items-center gap-4">
+        <img src="/images/logo.png" alt="logo" width="32px" />
+        <Link className="logo_text font-bold" to="/">CryptoHub</Link>
       </div>
 
-      <div className="hamburger">
+      <div className="hamburger bg-white text-black flex items-center justify-center">
         <Icon icon="material-symbols:menu" />
       </div>
     </header>
